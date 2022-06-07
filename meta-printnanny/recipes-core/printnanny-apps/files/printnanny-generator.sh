d6cfc9d0863d4e6b0b4085a34a31e60cef3d660b
Leigh Johnson <leigh@bitsy.ai>
2022-06-07 10:45:14 -0700

    log printnanny-generator to kernel msg buffer

--- meta-printnanny/recipes-core/printnanny-apps/files/printnanny-generator.sh
+++ meta-printnanny/recipes-core/printnanny-apps/files/printnanny-generator.sh
@@ -10,27 +10,28 @@ if [ "$SYSTEMD_FIRST_BOOT" == "1"]; then
 
     echo "[Unit]" > /etc/systemd/system/printnanny-dash.service.d/cookie-secret.conf
     echo -n "$COOKIE_SECRET" | systemd-creds encrypt --name cookie-secret -p - - >> "/etc/systemd/system/printnanny-dash.service.d/cookie-secret.conf"
-    echo "Created /etc/systemd/system/printnanny-dash.service.d/cookie-secret.conf"
+    echo "<4>printnanny-generator[$$]: Created /etc/systemd/system/printnanny-dash.service.d/cookie-secret.conf" > /dev/kmsg
 
     echo "[Unit]" > /etc/systemd/system/printnanny-dash.service.d/janus-edge-admin-secret.conf
     echo -n "$JANUS_ADMIN" | systemd-creds encrypt --name janus-edge-admin-secret -p - - >> "/etc/systemd/system/printnanny-dash.service.d/janus-edge-admin-secret.conf"
-    echo "Created /etc/systemd/system/printnanny-dash.service.d/janus-edge-admin-secret.conf"
+    echo "<4>printnanny-generator[$$]: Created /etc/systemd/system/printnanny-dash.service.d/janus-edge-admin-secret.conf" > /dev/kmsg
 
     echo "[Unit]" > /etc/systemd/system/printnanny-dash.service.d/janus-edge-api-token.conf
     echo -n "$JANUS_API_TOKEN" | systemd-creds encrypt --name janus-edge-api-token -p - - >> "/etc/systemd/system/printnanny-dash.service.d/janus-edge-api-token.conf"
-    echo "Created /etc/systemd/system/printnanny-dash.service.d/janus-edge-api-token.conf"
+    echo "<4>printnanny-generator[$$]: Created /etc/systemd/system/printnanny-dash.service.d/janus-edge-api-token.conf" > /dev/kmsg
 
     echo "[Unit]" > /etc/systemd/system/janus-gateway.service.d/janus-edge-admin-secret.conf
     echo -n "$JANUS_ADMIN" | systemd-creds encrypt --name janus-edge-admin-secret -p - - >> "/etc/systemd/system/janus-gateway.service.d/janus-edge-admin-secret.conf"
-    echo "Created /etc/systemd/system/janus-gateway.service.d/janus-edge-admin-secret.conf"
+    echo "<4>printnanny-generator[$$]: Created /etc/systemd/system/janus-gateway.service.d/janus-edge-admin-secret.conf" > /dev/kmsg
 
     echo "[Unit]" > /etc/systemd/system/janus-gateway.service.d/janus-edge-api-token.conf
     echo -n "$JANUS_API_TOKEN" | systemd-creds encrypt --name janus-edge-api-token -p - - >> "/etc/systemd/system/janus-gateway.service.d/janus-edge-api-token.conf"
-    echo "Created /etc/systemd/system/janus-gateway.service.d/janus-edge-api-token.conf"
-    echo "Initializing printnanny config"
+    echo "<4>printnanny-generator[$$]: Created /etc/systemd/system/janus-gateway.service.d/janus-edge-api-token.conf" > /dev/kmsg
     /usr/bin/printnanny config init --output=/etc/printnanny/default.toml
     mkdir -p /etc/printnanny/keys
+    echo "<4>printnanny-generator[$$]: Initialized printnanny config /etc/printnanny/default.toml" > /dev/kmsg
     /usr/bin/printnanny config generate-keys --output=/etc/printnanny/keys
+
 else
-    echo "Skipping printnanny config init, SYSTEMD_FIRST_BOOT=$SYSTEMD_FIRST_BOOT"
+    echo "<4>printnanny-generator[$$]: Skipping printnanny-generator, SYSTEMD_FIRST_BOOT=$SYSTEMD_FIRST_BOOT" > /dev/kmsg
 fi

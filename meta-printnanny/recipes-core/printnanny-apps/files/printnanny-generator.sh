6bc92ef52736fb1de983e3b41f95504ffd1a1eb8
Leigh Johnson <leigh@bitsy.ai>
2022-06-07 10:02:57 -0700

    move credential generators into SYSTEMD_FIRST_BOOT == 1 gate

--- meta-printnanny/recipes-core/printnanny-apps/files/printnanny-generator.sh
+++ meta-printnanny/recipes-core/printnanny-apps/files/printnanny-generator.sh
@@ -2,25 +2,31 @@
 
 set -euo pipefail
 
-COOKIE_SECRET=$(openssl rand -base64 32)
-JANUS_ADMIN=$(openssl rand -base64 32)
-JANUS_API_TOKEN=$(openssl rand -base64 32) 
-
-echo -n "$COOKIE_SECRET" | systemd-creds encrypt --name cookie-secret -p - - > "/etc/systemd/system/printnanny-dash.service.d/cookie-secret.conf"
-echo "Created /etc/systemd/system/printnanny-dash.service.d/cookie-secret.conf"
-echo -n "$JANUS_ADMIN" | systemd-creds encrypt --name janus-edge-admin-secret -p - - > "/etc/systemd/system/printnanny-dash.service.d/janus-edge-admin-secret.conf"
-echo "Created /etc/systemd/system/printnanny-dash.service.d/janus-edge-admin-secret.conf"
-echo -n "$JANUS_API_TOKEN" | systemd-creds encrypt --name janus-edge-api-token -p - - > "/etc/systemd/system/printnanny-dash.service.d/janus-edge-api-token.conf"
-echo "Created /etc/systemd/system/printnanny-dash.service.d/janus-edge-api-token.conf"
-
-mkdir -p /etc/systemd/system/janus-gateway.d/
-echo -n "$JANUS_ADMIN" | systemd-creds encrypt --name janus-edge-admin-secret -p - - > "/etc/systemd/system/janus-gateway.service.d/janus-edge-admin-secret.conf"
-echo "Created /etc/systemd/system/janus-gateway.service.d/janus-edge-admin-secret.conf"
-echo -n "$JANUS_API_TOKEN" | systemd-creds encrypt --name janus-edge-api-token -p - - > "/etc/systemd/system/janus-gateway.service.d/janus-edge-api-token.conf"
-echo "Created /etc/systemd/system/janus-gateway.service.d/janus-edge-api-token.conf"
-
 
 if [ "$SYSTEMD_FIRST_BOOT" == "1"]; then
+    COOKIE_SECRET=$(openssl rand -base64 32)
+    JANUS_ADMIN=$(openssl rand -base64 32)
+    JANUS_API_TOKEN=$(openssl rand -base64 32) 
+
+    echo "[Unit]" > /etc/systemd/system/printnanny-dash.service.d/cookie-secret.conf
+    echo -n "$COOKIE_SECRET" | systemd-creds encrypt --name cookie-secret -p - - >> "/etc/systemd/system/printnanny-dash.service.d/cookie-secret.conf"
+    echo "Created /etc/systemd/system/printnanny-dash.service.d/cookie-secret.conf"
+
+    echo "[Unit]" > /etc/systemd/system/printnanny-dash.service.d/janus-edge-admin-secret.conf
+    echo -n "$JANUS_ADMIN" | systemd-creds encrypt --name janus-edge-admin-secret -p - - >> "/etc/systemd/system/printnanny-dash.service.d/janus-edge-admin-secret.conf"
+    echo "Created /etc/systemd/system/printnanny-dash.service.d/janus-edge-admin-secret.conf"
+
+    echo "[Unit]" > /etc/systemd/system/printnanny-dash.service.d/janus-edge-api-token.conf
+    echo -n "$JANUS_API_TOKEN" | systemd-creds encrypt --name janus-edge-api-token -p - - >> "/etc/systemd/system/printnanny-dash.service.d/janus-edge-api-token.conf"
+    echo "Created /etc/systemd/system/printnanny-dash.service.d/janus-edge-api-token.conf"
+
+    echo "[Unit]" > /etc/systemd/system/janus-gateway.service.d/janus-edge-admin-secret.conf
+    echo -n "$JANUS_ADMIN" | systemd-creds encrypt --name janus-edge-admin-secret -p - - >> "/etc/systemd/system/janus-gateway.service.d/janus-edge-admin-secret.conf"
+    echo "Created /etc/systemd/system/janus-gateway.service.d/janus-edge-admin-secret.conf"
+
+    echo "[Unit]" > /etc/systemd/system/janus-gateway.service.d/janus-edge-api-token.conf
+    echo -n "$JANUS_API_TOKEN" | systemd-creds encrypt --name janus-edge-api-token -p - - >> "/etc/systemd/system/janus-gateway.service.d/janus-edge-api-token.conf"
+    echo "Created /etc/systemd/system/janus-gateway.service.d/janus-edge-api-token.conf"
     echo "Initializing printnanny config"
     /usr/bin/printnanny config init --output=/etc/printnanny/default.toml
     mkdir -p /etc/printnanny/keys

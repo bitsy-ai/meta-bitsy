72f9c4ecb6fe9bc96422de1544c6d91d128053c0
Leigh Johnson <leigh@bitsy.ai>
2022-06-10 13:24:50 -0700

    do not enable gstreamer service, fix quotes

--- recipes-multimedia/gstreamer-apps/files/gstreamer-rtph264pay.sh
+++ recipes-multimedia/gstreamer-apps/files/gstreamer-rtph264pay.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -x
 
-GSTREAMER_UDP_SINK_HOST="${GSTREAMER_UDP_SINK_HOST:-127.0.0.1}"
-GSTREAMER_UDP_SINK_PORT="${GSTREAMER_UDP_SINK_PORT:-5104}"
+export GSTREAMER_UDP_SINK_HOST="${GSTREAMER_UDP_SINK_HOST:-127.0.0.1}"
+export GSTREAMER_UDP_SINK_PORT="${GSTREAMER_UDP_SINK_PORT:-5104}"
 
 gst-launch-1.0 libcamerasrc \
     ! capsfilter caps=video/x-raw,width=640,height=480,format=NV12,framerate=10/1 \
@@ -12,4 +12,4 @@ gst-launch-1.0 libcamerasrc \
     ! 'video/x-h264,level=(string)4' \
     ! h264parse \
     ! rtph264pay \
-    ! "udpsink host=$GSTREAMER_UDP_SINK_HOST port=$GSTREAMER_UDP_SINK_PORT"
+    ! udpsink host="$GSTREAMER_UDP_SINK_HOST" port="$GSTREAMER_UDP_SINK_PORT"
--- recipes-multimedia/gstreamer-apps/gstreamer-apps.bb
+++ recipes-multimedia/gstreamer-apps/gstreamer-apps.bb
@@ -17,6 +17,7 @@ do_install() {
 }
 
 SYSTEMD_SERVICE:${PN} = "gstreamer-rtph264pay.service"
+SYSTEMD_AUTO_ENABLE = "disable"
 LICENSE_FLAGS_ACCEPTED += "commercial_gstreamer1.0-omx"
 RDEPENDS:${PN} = "\
     gstreamer1.0 \

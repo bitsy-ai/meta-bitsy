ebb8ccd88c2d6a1d4b64e2c7084516a9022a349d
Leigh Johnson <leigh@bitsy.ai>
2022-06-10 10:16:16 -0700

    fix quotes

--- recipes-multimedia/janus-gateway/files/janus-add-token.sh
+++ recipes-multimedia/janus-gateway/files/janus-add-token.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+echo "Reading credentials from $CREDENTIALS_DIRECTORY"
+
 JANUS_ADMIN_SECRET="$(cat ${CREDENTIALS_DIRECTORY}/janus-edge-admin-secret)"
 JANUS_API_TOKEN="$(cat ${CREDENTIALS_DIRECTORY}/janus-edge-api-token)"
+TRANSACTION="$(echo $RANDOM | md5sum | head -c 20; echo;)"
 
 curl -X POST http://127.0.0.1:7088/admin \
     -H 'Content-Type: application/json' \
-    -d '{"transaction" : "1acsf213","janus" : "add_token", "token": "$JANUS_API_TOKEN", "admin_secret": "$JANUS_ADMIN_SECRET"}'
+    -d @- <<EOF
+{"transaction" : "$TRANSACTION","janus" : "add_token", "token": "$JANUS_API_TOKEN", "admin_secret": "$JANUS_ADMIN_SECRET"}
+EOF

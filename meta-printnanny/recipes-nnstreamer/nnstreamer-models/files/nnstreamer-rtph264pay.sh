f203a12b37470db6ad71017ddbb24786d5824e68
Leigh Johnson <leigh@bitsy.ai>
2022-06-11 12:35:18 -0700

    create nnstreamer.service

--- meta-printnanny/recipes-nnstreamer/nnstreamer-models/files/nnstreamer-rtph264pay.sh
+++ meta-printnanny/recipes-nnstreamer/nnstreamer-models/files/nnstreamer-rtph264pay.sh
@@ -7,7 +7,7 @@ export GSTREAMER_UDP_SINK_PORT_OVERLAY="${GSTREAMER_UDP_SINK_PORT:-5106}"
 export GSTREAMER_UDP_SINK_PORT_DATA="${GSTREAMER_UDP_SINK_PORT:-5107}"
 
 
-gst-launch-1.0 -v libcamerasrc \
+gst-launch-1.0 --quiet --messages --no-position libcamerasrc \
     ! tee name=t \
     t.  ! queue leaky=2 max-size-buffers=2 \
         ! v4l2convert \
@@ -26,7 +26,7 @@ gst-launch-1.0 -v libcamerasrc \
         ! rtph264pay \
         ! udpsink host="$GSTREAMER_UDP_SINK_HOST" port="$GSTREAMER_UDP_SINK_PORT_OVERLAY" \
     t.  ! queue leaky=2 max-size-buffers=2 \
-        ! capsfilter caps=video/x-raw,width=640,height=480,format=RGB
+        ! capsfilter caps=video/x-raw,width=640,height=480,format=RGB \
         ! v4l2convert \
         ! v4l2h264enc extra-controls="controls,repeat_sequence_header=1" \
         ! 'video/x-h264,width=640,height=480,level=(string)4' \

62eb05d6509d5aae9deff3ef00336f58bd187f6f
Leigh Johnson <leigh@bitsy.ai>
2022-06-16 21:21:25 -0700

    fix var (#89)

--- meta-printnanny/recipes-extended/cloud-init/cloud-init/001-fix-perms.sh
+++ meta-printnanny/recipes-extended/cloud-init/cloud-init/001-fix-perms.sh
@@ -23,7 +23,7 @@ FIX_PRINTNANNY_GROUP=(
     "/etc/printnanny"
 )
 
-for f in "${FIX_VIDEO_GROUP[@]}"
+for f in "${FIX_PRINTNANNY_GROUP[@]}"
     chmod -R u=rwx,g=rwx,o=rx "$f"
     chown -R :printnanny "$f"
 do

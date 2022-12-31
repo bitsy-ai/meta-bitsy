de39efdf2276ddde93c825808c5ede30267af9d2
Leigh Johnson <leigh@bitsy.ai>
2022-12-30 19:42:00 -0800

    Run home directory chown as per-boot cloud-init script (#335)
    * move printnanny-link-confd script contents into cloud-init once-per-boot script
    
    * fix: per-once, per-instance, per-boot scripts missing from cloud-init installation

--- meta-printnanny/recipes-extended/cloud-init/cloud-init/scripts/per-boot/001-run-once-per-boot.sh
+++ meta-printnanny/recipes-extended/cloud-init/cloud-init/scripts/per-boot/001-run-once-per-boot.sh
@@ -1,5 +1,23 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -euo pipefail
 
-echo "Running cloudinit once-per-boot scripts"
+FIX_PRINTNANNY_GROUP=(
+    "/etc/printnanny"
+)
+
+FIX_PRINTNANNY_USER_GROUP=(
+    "/home/printnanny"
+)
+
+for f in "${FIX_PRINTNANNY_GROUP[@]}"
+do
+    chmod -R u=rwx,g=rwx,o=rx "$f"
+    chown -R :printnanny "$f"
+done
+
+for f in "${FIX_PRINTNANNY_USER_GROUP[@]}"
+do
+    chmod -R u=rwx,g=rwx,o=rx "$f"
+    chown -R printnanny:printnanny "$f"
+done
